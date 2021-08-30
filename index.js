c5cd10eb4f38e0287109e07e0436370719e0a95f
Amit Gujar <amitgujar3342@gmail.com>
2021-08-31 00:11:36 +0530

    added help cmnd

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { getRandomFacts } = require("allfacts");
 const giveMeAJoke = require("discord-jokes");
-// const discord = require("discord.js"); 
-const { Client, Intents, Permissions} = require('discord.js');
+const { Client, Intents, Permissions, MessageEmbed } = require("discord.js");
 var catMe = require("cat-me");
 const express = require("express");
 const app = express();
 const prefix = "!";
-// const client = new discord.Client();
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 require("dotenv").config();
 
 app.listen(process.env.PORT || 3000, () => {
@@ -19,7 +19,7 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", (messageCreate) => {
-  client.user.setActivity(`Discord`, { type: "PLAYING" })
+  client.user.setActivity(`!help`, { type: "PLAYING" }); 
   if (messageCreate.author.bot) return;
   if (!messageCreate.content.startsWith(prefix)) {
     console.log(messageCreate.author);
@@ -40,9 +40,45 @@ client.on("messageCreate", (messageCreate) => {
     });
   } else if (command === "cat") {
     messageCreate.channel.send(catMe());
+  } else if (command === "help") {
+    // shows help
+    const helpEmbed = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle("Dummy Help")
+      .setDescription("My prefix for commands is !")
+      .setThumbnail("https://i.imgur.com/r4t0Yve.png")
+      .addFields(
+        {
+          name: "Command",
+          value: `
+      !help
+      !kick @user
+      !ping
+      !fact
+      !joke
+      !cat
+    `,
+          inline: true,
+        },
+        {
+          name: "Utility",
+          value: `
+      - Display this message
+      - Kick users from server
+      - Shows ping for message
+      - Shows random fact around the world
+      - You will see how good is dummy at jokes
+      - Shows random ASCII cat 😊
+    `,
+          inline: true,
+        }
+      )
+      .setTimestamp()
+      .setFooter("More commands will be added soon.");
+
+    messageCreate.channel.send({ embeds: [helpEmbed] });
   }
 });
-
 
 client.on("messageCreate", (messageCreate) => {
   if (!messageCreate.guild) return;
@@ -53,12 +89,14 @@ client.on("messageCreate", (messageCreate) => {
       // Now we get the member from the user
       const member = messageCreate.guild.members.resolve(user);
       // check for user permission to kick member
-      if (messageCreate.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) {
+      if (
+        messageCreate.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)
+      ) {
         // if user is me or dummy
         if (user.discriminator === "7504" || user.discriminator === "4456") {
           messageCreate.reply("Wtf u doing, I can't kick myself");
         }
-        // If the member is in the guild
+        // If the member is in the server
         else if (member) {
           member
             .kick()
@@ -67,14 +105,14 @@ client.on("messageCreate", (messageCreate) => {
               console.log(user);
             })
             .catch((err) => {
-              messageCreate.reply("Looks like you don't have permission");
+              messageCreate.reply("That user is a mod/admin, I can't do that.");
               console.error(err);
             });
         } else {
           messageCreate.reply("That user isn't in this guild!");
         }
       } else {
-        messageCreate.reply("Nice try, but u dont have permission lol.")
+        messageCreate.reply("Nice try, but u dont have permission lol.");
       }
     } else {
       messageCreate.reply("You didn't mention the user to kick!");
